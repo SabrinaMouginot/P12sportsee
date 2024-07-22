7d893d40f31e167cc32f69b1bc00eba7ddf0c55b
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 22:11:21 +0200

    travail de style sur le footer  :  le copyright est ecrit en vertical

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -7,13 +7,13 @@ import '../css/Footer.css';
 function Footer() {
     return (
         <div className="Footer">
-            <ul>
+            <ul className='Sport'>
                 <li><img src={yoga} alt="image d'un personnage qui fait du yoga"/></li>
                 <li><img src={swim} alt="image d'un nageur" /></li>
                 <li><img src={bike} alt="image d'un coureur-cycliste" /></li>
                 <li><img src={bodybuilding} alt="image d'un haltère" /></li>
             </ul>
-            Copyright, SportSee 2024
+            <p>Copyright, SportSee 2024</p>
         </div>
     );
 }

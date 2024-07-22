976a01efbf20e8f6c07173a892810e5d993f86f8
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 23:45:03 +0200

    les éléments du header sont cliquables

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,10 +11,10 @@ function Header() {
                     <img src={sportsee} alt="SportSee" />
                 </div>
                 <ul className='NavItems'>
-                    <li>Accueil</li>
-                    <li>Profil</li>
-                    <li>Réglage</li>
-                    <li>Communauté</li>
+                    <li><a href="#">Accueil</a></li>
+                    <li><a href="#">Profil</a></li>
+                    <li><a href="#">Réglage</a></li>
+                    <li><a href="#">Communauté</a></li>
                 </ul>
             </nav>
         </div>

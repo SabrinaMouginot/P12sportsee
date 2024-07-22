cabf5252fecf35e7e7060d7c75ca6efc8cf40aa8
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 18:07:53 +0200

    insertion des images contenues dans le footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,19 @@
+import bodybuilding from '../assets/bodybuilding.svg';
+import bike from '../assets/bike.svg';
+import swim from '../assets/swim.svg';
+import yoga from '../assets/yoga.svg';
+
 function Footer() {
     return (
-            <div className="Header">
-                <ul>
-                    <li>Copyright, SportSee 2024</li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-            </div>
+        <div className="Footer">
+            <ul>
+                <li><img src={yoga} alt="image d'un personnage qui fait du yoga"/></li>
+                <li><img src={swim} alt="image d'un nageur" /></li>
+                <li><img src={bike} alt="image d'un coureur-cycliste" /></li>
+                <li><img src={bodybuilding} alt="image d'un haltère" /></li>
+                <li>Copyright, SportSee 2024</li>
+            </ul>
+        </div>
     );
 }
 

75f91e74a3b3d5b59e998e398e818fc2e419682e
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 17:29:09 +0200

    creation du contenu du footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+    return (
+            <div className="Header">
+                <ul>
+                    <li>Copyright, SportSee 2024</li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </div>
+    );
+}
+
+export default Footer;

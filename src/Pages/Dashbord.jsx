cd17ce8ebfcba2cdb7a980416a7ba0b3044adfc2
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 15:40:36 +0200

    creation de la page Dashbord

---
+++ src/Pages/Dashbord.jsx
@@ -0,0 +1,19 @@
+
+
+
+function Dashbord() {
+    return (
+        <div className="App">
+            <div className="Header">
+                <ul>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglage</li>
+                    <li>Communauté</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Dashbord;

44156b3ea3eedcc9abffbe785e24d60dad919f2a
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-22 17:28:52 +0200

    creation du contenu du Header

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+            <div className="Header">
+                <ul>
+                    <li>Accueil</li>
+                    <li>Profil</li>
+                    <li>Réglage</li>
+                    <li>Communauté</li>
+                </ul>
+            </div>
+    );
+}
+
+export default Header;

064d725e381b03c3507bfafbaf6f34c114e6f876
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-24 16:56:35 +0200

    insertion de la légende du poids dans le graph Activite quotidienne

--- src/Components/Charts/ActiviteQuotidienne.jsx
+++ src/Components/Charts/ActiviteQuotidienne.jsx
@@ -1,8 +1,13 @@
 import '../../css/ActiviteQuotidienne.css';
+import Oval from '../../assets/Oval.svg';
 
 function ActiviteQuotidienne() {
     return (
-        <div className="activity">Activité quotidienne</div>
+        <div className="activity">
+            <h2>Activité quotidienne</h2>
+            <p><img src= {Oval} alt="pastille bleu" /> Poids (kg)</p>
+            
+        </div>
     );
 }
 

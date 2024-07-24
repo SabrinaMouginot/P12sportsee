2112d353a1104bf177a14377f729e1ccd3e76224
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-24 15:24:20 +0200

    insertion de l img lipides dans les nutriments

--- src/Components/NutrimentsCards/Lipides.jsx
+++ src/Components/NutrimentsCards/Lipides.jsx
@@ -1,8 +1,12 @@
 import '../../css/Lipides.css'
+import lipides from '../../assets/nutriments/fat-icon.svg';
 
 function Lipides() {
     return (
-        <div className="lipides">Lipides</div>
+        <div className="lipides">
+            <img src={lipides} alt="représentation des lipides" />
+            <p>Lipides</p>
+        </div>
     );
 }
 

84afa7a4de961a4e9d52e691e54f3251be1d2cef
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-26 14:47:13 +0200

    réorganisation des fichiers - reintroduction des Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import './App.css'
-import Dashbord from './Pages/Dashbord'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Dashbord from './Pages/Dashbord';
+import './App.css';
 
 function App() {
-  
   return (
-    <div>
-      <Dashbord/>
-    </div>
-  )
+    <Router>
+      <div className="App">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Dashbord />} />
+            {/* Ajouter d'autres routes ici si nécessaire */}
+          </Routes>
+        </div>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;

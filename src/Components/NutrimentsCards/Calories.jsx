0cef01e6334008e336d9284de2407db1731cb769
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-26 19:26:44 +0200

    simplification des fichiers Calories Proteines Glucides Lipides

--- src/Components/NutrimentsCards/Calories.jsx
+++ src/Components/NutrimentsCards/Calories.jsx
@@ -1,15 +1,19 @@
-import '../../css/Calories.css';
-import calories from '../../assets/nutriments/calories-icon.svg';
+// import '../../css/Calories.css';
+// import calories from '../../assets/nutriments/calories-icon.svg';
+import Nutriment from './Nutriment';
+import caloriesIcon from '../../assets/nutriments/calories-icon.svg';
+import '../../css/Nutriment.css'
 
 function Calories() {
     return (
-        <div className="calories">
-            <img src={calories} alt="représentation des calories" />
-            <div className="text-container">
-                <p className='valeurs'>1,930kCal</p>
-                <p>Calories</p>
-            </div>
-        </div>
+        // <div className="calories">
+        //     <img src={calories} alt="représentation des calories" />
+        //     <div className="text-container">
+        //         <p className='valeurs'>1,930kCal</p>
+        //         <p>Calories</p>
+        //     </div>
+        // </div>
+        <Nutriment icon={caloriesIcon} value="1,930kCal" label="Calories" />
     );
 }
 
--- src/Components/NutrimentsCards/Glucides.jsx
+++ src/Components/NutrimentsCards/Glucides.jsx
@@ -1,15 +1,18 @@
-import '../../css/Glucides.css'
-import glucides from '../../assets/nutriments/carbs-icon.svg';
+// import '../../css/Glucides.css'
+// import glucides from '../../assets/nutriments/carbs-icon.svg';
+import Nutriment from './Nutriment';
+import glucidesIcon from '../../assets/nutriments/carbs-icon.svg';
 
 function Glucides() {
     return (
-        <div className="glucides">
-            <img src={glucides} alt="représentation des glucides" />
-            <div className="text-container">
-                <p className='valeurs'>290g</p>
-                <p>Glucides</p>
-            </div>
-        </div>
+        // <div className="glucides">
+        //     <img src={glucides} alt="représentation des glucides" />
+        //     <div className="text-container">
+        //         <p className='valeurs'>290g</p>
+        //         <p>Glucides</p>
+        //     </div>
+        // </div>
+        <Nutriment icon={glucidesIcon} value="290g" label="Glucides" />
     );
 }
 

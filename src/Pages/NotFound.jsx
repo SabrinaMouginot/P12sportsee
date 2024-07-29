f9608de959dec6d6d6daee3993ca7fb4d90f2887
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-29 21:06:00 +0200

    travail de style sur la page 404

--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,17 +1,3 @@
-// import '../css/NotFound.css';
-
-// const NotFound = () => {
-//   return (
-//     <div className='notfound-msg'>
-//       <h1 className='notfound-title'>404</h1>
-//       <p>Oups! La page que vous demandez n'existe pas.</p>
-//       <a href="/">Retourner vers la page d'accueil</a>
-//     </div>
-//   );
-// };
-
-// export default NotFound;
-
 import { useNavigate } from 'react-router-dom';
 import '../css/NotFound.css';
 
@@ -21,8 +7,8 @@ const NotFound = () => {
   return (
     <div className='notfound-msg'>
       <h1 className='notfound-title'>404</h1>
-      <p>Oups! La page que vous demandez n'existe pas.</p>
-      <button onClick={() => navigate(-1)}>Retourner vers la page précédente</button>
+      <p>Oups! La page que vous demandez n&#39;existe pas.</p>
+      <span className='back-link' onClick={() => navigate(-1)}>Retourner à la page précédente</span>
     </div>
   );
 };

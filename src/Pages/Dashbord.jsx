94764411e9eefbdb10a94ebc02098c1ef1e187e6
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-29 13:03:30 +0200

    suppression des fichiers calories proteines glucides lipides qui contenait une seule ligne ( inserees directement dans le dashbord )

--- src/Pages/Dashbord.jsx
+++ src/Pages/Dashbord.jsx
@@ -1,39 +1,46 @@
-import ActiviteQuotidienne from "../Components/Charts/ActiviteQuotidienne";
+import ActivityBarChart from "../Components/Charts/charts/ActivityBarCharts";
 import Performance from "../Components/Charts/Performance";
 import Duree from "../Components/Charts/SessionMoyenne";
 import Score from "../Components/Charts/Score";
-import Calories from "../Components/NutrimentsCards/Calories";
-import Glucides from "../Components/NutrimentsCards/Glucides";
-import Lipides from "../Components/NutrimentsCards/Lipides";
-import Proteines from "../Components/NutrimentsCards/Proteines";
+import Nutriment from "../Components/NutrimentsCards/Nutriment";
+import caloriesIcon from '../assets/nutriments/calories-icon.svg';
+import glucidesIcon from '../assets/nutriments/carbs-icon.svg';
+import lipidesIcon from '../assets/nutriments/fat-icon.svg';
+import proteinesIcon from '../assets/nutriments/protein-icon.svg';
+import { useParams } from "react-router";
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import '../css/Dashbord.css';
 
 function Dashbord() {
-    return (
-        <div className="dashbord">
-            <div className="container">
-                <div className="txtContainer">
-                <h1>Bonjour <span className="prenom">Thomas</span></h1>
-                <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
-                </div>
-                <div className="grid-container">
-                    <ActiviteQuotidienne />
-                    <div className="nutiments">
-                        <Calories />
-                        <Proteines />
-                        <Glucides />
-                        <Lipides />
-                    </div>
-                    <div className="bottom-row">
-                        <Duree />
-                        <Performance />
-                        <Score />
-                    </div>
-                </div>
-            </div>
 
-        </div>
-    );
-}
+
+        return (
+            <div className="dashbord">
+                <div className="container">
+                    <div className="txtContainer">
+                        <h1>Bonjour <span className="prenom">{data.userInfos.firstName}</span></h1>
+                        <p>Félicitation ! Vous avez explosé vos objectifs hier 👏</p>
+                    </div>
+                    <div className="grid-container">
+                        <ActivityBarChart userId={userId} />
+                        <div className="nutiments">
+                            <Nutriment icon={caloriesIcon} value="1,930kCal" label="Calories" />
+                            <Nutriment icon={proteinesIcon} value="155g" label="Protéines" />
+                            <Nutriment icon={glucidesIcon} value="290g" label="Glucides" />
+                            <Nutriment icon={lipidesIcon} value="50g" label="Lipides" />
+                        </div>
+                        <div className="bottom-row">
+                            <Duree />
+                            <Performance />
+                            <Score />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+
 
 export default Dashbord;

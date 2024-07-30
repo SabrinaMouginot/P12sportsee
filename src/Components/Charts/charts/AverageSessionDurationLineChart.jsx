88dc452288e00e7025a9ce333e8f906452a30b15
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-07-30 20:41:03 +0200

    AverageSessionDurationLineChart - recherche pq la courbe ne saffichait pas - introduction type de ligne et datakey

--- src/Components/Charts/charts/AverageSessionDurationLineChart.jsx
+++ src/Components/Charts/charts/AverageSessionDurationLineChart.jsx
@@ -8,21 +8,23 @@ function AverageSessionDurationLineChart ({userId}) {
     useEffect(() => {
         axios.get(`http://localhost:3000/user/${userId}/average-sessions`)
         .then(response => {
+            console.log('API Response:', response.data); 
             setData(response.data.data.sessions);
         })
             .catch(error => console.error('Error fetching data:', error));
     }, [userId]);
 
     return (
-        <ResponsiveContainer width="100%" height={250}>
+        <ResponsiveContainer width="30%" height={250}>
             <LineChart data={data}>
                 <CartesianGrid strokeDasharray={false} vertical={false} />
                 <XAxis dataKey="day" />
                 <Tooltip />
-                <Line/>
+                <Line type="monotone" dataKey="sessionLength" stroke="#FBFBFB"/>
             </LineChart>
         </ResponsiveContainer>
     );
 }
+
 
 export default AverageSessionDurationLineChart;

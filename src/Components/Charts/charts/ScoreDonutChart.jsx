2222bad4edef45f962dda933cc7582cf80863955
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-02 12:43:05 +0200

    ScoreDonutChart.jsx - récupérer les données de l API

--- src/Components/Charts/charts/ScoreDonutChart.jsx
+++ src/Components/Charts/charts/ScoreDonutChart.jsx
@@ -1,14 +1,33 @@
+import { useEffect, useState } from 'react';
 import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 import '../../../sass/ScoreDonutChart.scss';
+import axios from 'axios';
 
 function ScoreDonutChart({ userId }) {
+    const [data, setdata] = useState(0); // État pour stocker le score
+    
+    useEffect(() => {
+        // Appel API pour récupérer les données utilisateur
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/user/${userId}`);
+                setdata(response.data.data);
+    
+            } catch (error) {
+                console.error('Erreur lors de la récupération des données:', error);
+            }
+        };
+    
+        fetchData();
+    }, [userId]);
+    
 
     return (
         <div className="chart-wrapper">
             <h2 className="title">Score</h2>
             <ResponsiveContainer width="100%" height={250}>
                 <PieChart>
-                    <Pie dataKey="value">
+                    <Pie data={data} dataKey="value">
                     </Pie>
                     <text
                         x="50%"

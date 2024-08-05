1b20db8289d7e23c86b99f2ecf040494d8da26a9
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-05 13:57:11 +0200

    modélisation des donnees

---
+++ src/API/user.model.js
@@ -0,0 +1,17 @@
+export class UserData {
+    firstName
+    score
+    calorieCount
+    proteinCount
+    carbohydrateCount
+    lipidCount
+
+    constructor(firstName, score, calorieCount, proteinCount, carbohydrateCount, lipidCount) {
+        this.firstName = firstName
+        this.score = (score * 100).toFixed(0)
+        this.calorieCount = calorieCount
+        this.proteinCount = proteinCount
+        this.carbohydrateCount = carbohydrateCount
+        this.lipidCount = lipidCount
+    }
+}

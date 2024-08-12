82834837ba59492f1d8feec81ec55f007d791bab
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-08-12 14:32:38 +0200

    conditionnement du rendu dans la fonction ActivityBarChart

--- src/Components/Charts/charts/ActivityBarChart.jsx
+++ src/Components/Charts/charts/ActivityBarChart.jsx
@@ -30,23 +30,23 @@ function ActivityBarChart({ userId }) {
             PropTypes.shape({  // shape définit la forme spécifique de l'objet
                 value: PropTypes.number.isRequired, // Valide que `value` est un nombre
             })
-        ).isRequired,
+        ),
     };
 
     return (
         <div className="chart-wrapper activity">
             <h2 className="title">Activité quotidienne</h2>
-            <ResponsiveContainer width="100%" height={250}>
-                <BarChart data={data} barGap={10} barCategoryGap={50}>
+            <ResponsiveContainer width="100%">
+                {data.length && <BarChart data={data} barGap={10} barCategoryGap={50}>
                     <CartesianGrid strokeDasharray="3 3" vertical={false} stroke='#DEDEDE' />
-                    <XAxis 
-                        dataKey="day" 
+                    <XAxis
+                        dataKey="day"
                         tickFormatter={(value, index) => index + 1} // Remplace les dates par une numérotation
                         axisLine={false} // Supprime la ligne de l'axe des abscisses
                         tickLine={false} // Supprime les petits traits de l'axe des abscisses
                     />
-                    <YAxis 
-                        orientation="right" 
+                    <YAxis
+                        orientation="right"
                         axisLine={false} // Supprime la ligne de l'axe des ordonnées
                         tickLine={false} // Supprime les petits traits de l'axe des ordonnées
                         tick={{ fill: '#9B9EAC', fontSize: 12 }} // Style des valeurs de l'axe des ordonnées
@@ -58,9 +58,9 @@ function ActivityBarChart({ userId }) {
                         align="right"
                         wrapperStyle={{ top: -29, right: 0 }}
                     />
-                    <Bar dataKey="kilogram" fill="#282D30" name="Poids (kg)" barSize={10} radius={[4, 4, 0, 0]}/>
-                    <Bar dataKey="calories" fill="#E60000" name="Calories brûlées (kCal)" barSize={10} radius={[4, 4, 0, 0]}/>
-                </BarChart>
+                    <Bar dataKey="kilogram" fill="#282D30" name="Poids (kg)" barSize={10} radius={[4, 4, 0, 0]} />
+                    <Bar dataKey="calories" fill="#E60000" name="Calories brûlées (kCal)" barSize={10} radius={[4, 4, 0, 0]} />
+                </BarChart>}
             </ResponsiveContainer>
         </div>
     );
